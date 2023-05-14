a6a08d90c0a67d5d56bd677440b56ee3a103ac29
raulcondori <raul_condori@usmp.pe>
2023-05-14 14:11:54 -0500

    PrimerAvance

---
+++ Semaforo.ino
@@ -0,0 +1,31 @@
+ const int LED_ROJO 		= 5;
+ const int LED_AMARILLO 	= 6;
+ const int LED_VERDE		= 7;
+
+void setup() {
+   	pinMode(LED_ROJO, OUTPUT);
+   	pinMode(LED_AMARILLO, OUTPUT);
+   	pinMode(LED_VERDE, OUTPUT);
+}
+
+void loop() {
+   for(int i = 0; i < 3; i++) {
+       	if(i == 0) {
+          digitalWrite(LED_VERDE, HIGH);
+          delay(30000);
+      	}
+     	else if(i == 1) {
+          digitalWrite(LED_VERDE, LOW);
+          digitalWrite(LED_AMARILLO, HIGH);
+          delay(5000);
+      	}
+      	else if(i == 2) {
+          digitalWrite(LED_AMARILLO, LOW);
+          digitalWrite(LED_ROJO, HIGH);
+          delay(30000);
+      	}
+    }
+    digitalWrite(LED_ROJO, LOW);
+    digitalWrite(LED_AMARILLO, HIGH);
+    delay(5000);
+  }
